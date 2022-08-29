02d11e9c3ebb0b65632782e31271ea4af00cce61
ignacioborraz <ignacioborraz@hotmail.com>
2022-08-29 18:17:58 -0300

    get one event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -16,6 +16,32 @@ const eventController = {
                 success: false
             })
         }
+    },
+    read: async(req,res) => {
+        const {id} = req.params
+        try {
+            let event = await Event.findOne({_id:id})
+            //si event existe retorno un json con los datos
+            //si event no existe => event = {} retorno un json con 404
+            if (event) {
+                res.status(200).json({
+                    message: "you get one event",
+                    response: event,
+                    success: true
+                })
+            } else {
+                res.status(404).json({
+                    message: "could't find event",
+                    success: false
+                })
+            }
+        } catch(error) {
+            console.log(error)
+            res.status(400).json({
+                message: "error",
+                success: false
+            })
+        }
     }
 }
 
