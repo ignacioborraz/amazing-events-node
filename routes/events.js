f7bd8c1aca3e5d15cf58624912ed6db734d60359
alejandro <info@alezvi.com.ar>
2022-08-30 17:31:07 -0300

    new method

--- routes/events.js
+++ routes/events.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const {
+    all,
     create,
     read
 } = require('../controllers/eventController')
 
+router.get('/', all)
 router.get('/:id', read)
 router.post('/', create)
 
