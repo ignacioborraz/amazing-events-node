dfd406160c8ba675cfedb81a8f576f9c40857fec
ignacioborraz <ignacioborraz@hotmail.com>
2022-09-05 13:20:12 -0300

    routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 const eventRouter = require('./events')
+const userRouter = require('./users')
+//const commentRouter = require('./comments')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Amazing Events' });
 });
 
 router.use('/events', eventRouter)
+router.use('/users', userRouter)
+//router.use('/comments', commentRouter)
 
 module.exports = router;
