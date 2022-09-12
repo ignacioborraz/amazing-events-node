f196cc7e0a4d9fb07ecc17d8eab836f933ed359f
ignacioborraz <ignacioborraz@hotmail.com>
2022-09-12 11:51:21 -0300

    verify mail

--- controllers/userController.js
+++ controllers/userController.js
@@ -97,7 +97,36 @@ const userController = {
         }
     },
 
-    verifyMail: async() => {},
+    //el codigo unico y aleatorio generado en el metodo de signup
+    //se pasa por params a este otro metodo para poder verificar la cuenta
+    //luego de requerirlo lo comparo con los perfiles ya creados (lo busco en la base de datos)
+        //si encuentra el usuario cambio el verified de false a true
+        //si no lo encuentra avisar que el mail a verificar no tiene cuenta
+    verifyMail: async(req,res) => {
+        const {code} = req.params
+        //let user = await User.findOne({code:code})
+        try {
+            let user = await User.findOne({code})
+            if (user) {
+                user.verified = true //cambio la propiedad
+                await user.save() //guardo el cambio en la base de datos
+                res.redirect('https://www.google.com') //aqui se coloca el link de redireccionamiento
+                //puede redireccionar hacia index/login o hacia una pagina nueva de bienvenida al usuario nuevo
+                //NO OLVIDAR HOSTEAR EL FRONT PARA QUE FUNCIONE EL REDIRECCIONAMIENTO
+            } else {
+                res.status(404).json({
+                    message: "email has not account yet",
+                    success: false
+                })
+            }
+        } catch(error) {
+            console.log(error)
+            res.status(400).json({
+                message: "could't verify account",
+                success: false
+            })
+        }
+    },
     
     signIn: async() => {},
 
