3e13f43db5007e469a848afdd07748d46997a0ba
ignacioborraz <ignacioborraz@hotmail.com>
2022-09-13 22:30:07 -0300

    validations

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,5 +1,36 @@
 const Comment = require('../models/Comment')
 
+const Joi = require('joi')
+
+const validator = Joi.object({
+    "comment" : Joi.string()
+        .required()
+        .min(3)
+        .messages({
+            'any.required': 'COMMENT_REQUIRED',
+            'string.empty': 'COMMENT_REQUIRED',
+            'string.min': 'COMMENT_TOO_SHORT',
+        }),
+    "user": Joi.string()
+        .required()
+        .messages({
+            'any.required': 'USER_REQUIRED',
+            'string.empty': 'USER_REQUIRED',
+        }),
+    "event": Joi.string()
+        .required()
+        .messages({
+            'any.required': 'EVENT_REQUIRED',
+            'string.empty': 'EVENT_REQUIRED',
+        }),
+    "date": Joi.string()
+        .required()
+        .messages({
+            'any.required': 'DATE_REQUIRED',
+            'string.empty': 'DATE_REQUIRED',
+        }),
+})
+
 const commentController = {
 
     create: async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
+    
     name: {
         type: String,
-        required: true
+        required: true,
     },
     photo: {
         type: String,
@@ -13,57 +14,31 @@ const schema = new mongoose.Schema({
         type: String,
         required: true
     },
-
-    /**
-     * TIENE QUE que ser un array para que pueda alojar TODAS LAS CONTRASEÑAS historicas
-     */
     pass: [{
         type: String,
         required: true
     }],
-
-    /**
-     * como usuario comun vamos a tener que asignarle el rol "user"
-     * hay que ver la forma de crear administradores de otra forma
-     */
     role: {
         type: String,
         required: true
     },
-
-    /**
-     * array con todas las formas en las que se registró el usuario
-     */
     from: [{
         type: String,
         required: true
     }],
-
-    /**
-     * por defecto como el usuario no está logueado va a estar definido como false
-     * y cuando el usuario se loguea cambia a true
-     */
     logged: {
         type: Boolean,
         required: true
     },
-
-    /**
-     * por defecto como el usuario no está verificado va a estar definido como false
-     * cuando el usuario verifique su cuenta cambia a true
-     */
     verified: {
         type: Boolean,
         required: true
     },
-
-    /**
-     * codigo de verificacion del usuario que "se enviará" por mail
-     */
     code: {
         type: String,
         required: true
     }
+
 })
 
 const User = mongoose.model('users', schema)
