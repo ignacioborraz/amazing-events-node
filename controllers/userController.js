1183636fcb8d5423f11b4852c5104b614d1190e3
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2022-09-13 23:42:34 -0300

    Validations (#31)
    * Sprint 3 (#27)
    
    * B18-user (#20)
    
    * ignoring .env and node_modules
    
    * app scaffolding
    
    * updates
    
    * routes definition examples
    
    * including new routes
    
    * express generator
    
    * example of enviroment variable
    
    * adding .env
    
    * adding dotenv
    
    * console.log of port
    
    * adding libreries
    
    * conection to mongo with mongoose
    
    * new model
    
    * adding indexRouter
    
    * adding eventRouter
    
    * create event
    
    * eventRouter
    
    * get one event
    
    * update and destroy methods
    
    * code style fixes
    
    * new route for listing all events
    
    * new method
    
    * population script example
    
    * VSCode configs directory added
    
    * CORS middleware added
    
    * fixed model property
    
    * fixed array of populate
    
    * model
    
    * controller
    
    * routes
    
    * populate
    
    Co-authored-by: alejandro <info@alezvi.com.ar>
    Co-authored-by: Eric Rodriguez <67911854+ericfrodriguez@users.noreply.github.com>
    Co-authored-by: Eric Rodriguez <feric.rodriguez@gmail.com>
    
    * B19-comment (#22)
    
    * ignoring .env and node_modules
    
    * app scaffolding
    
    * updates
    
    * routes definition examples
    
    * including new routes
    
    * express generator
    
    * example of enviroment variable
    
    * adding .env
    
    * adding dotenv
    
    * console.log of port
    
    * adding libreries
    
    * conection to mongo with mongoose
    
    * new model
    
    * adding indexRouter
    
    * adding eventRouter
    
    * create event
    
    * eventRouter
    
    * get one event
    
    * update and destroy methods
    
    * code style fixes
    
    * new route for listing all events
    
    * new method
    
    * population script example
    
    * VSCode configs directory added
    
    * CORS middleware added
    
    * fixed model property
    
    * fixed array of populate
    
    * model
    
    * controller
    
    * routes
    
    * populate
    
    * model+controller+routes of 'comment'
    
    * user
    
    * date
    
    Co-authored-by: alejandro <info@alezvi.com.ar>
    Co-authored-by: Eric Rodriguez <67911854+ericfrodriguez@users.noreply.github.com>
    Co-authored-by: Eric Rodriguez <feric.rodriguez@gmail.com>
    
    * issue-comments (#23)
    
    * ref
    
    * populating+querys
    
    * model+controller of user
    
    * send mail
    
    * verify mail
    
    * Api testing (#26)
    
    * ignoring .env and node_modules
    
    * app scaffolding
    
    * updates
    
    * routes definition examples
    
    * including new routes
    
    * express generator
    
    * example of enviroment variable
    
    * adding .env
    
    * adding dotenv
    
    * console.log of port
    
    * adding libreries
    
    * conection to mongo with mongoose
    
    * new model
    
    * adding indexRouter
    
    * adding eventRouter
    
    * create event
    
    * eventRouter
    
    * get one event
    
    * update and destroy methods
    
    * code style fixes
    
    * new route for listing all events
    
    * new method
    
    * population script example
    
    * VSCode configs directory added
    
    * CORS middleware added
    
    * fixed model property
    
    * fixed array of populate
    
    * cors
    
    * api testing
    
    Co-authored-by: alejandro <info@alezvi.com.ar>
    Co-authored-by: Eric Rodriguez <67911854+ericfrodriguez@users.noreply.github.com>
    Co-authored-by: Eric Rodriguez <feric.rodriguez@gmail.com>
    
    Co-authored-by: alejandro <info@alezvi.com.ar>
    Co-authored-by: Eric Rodriguez <67911854+ericfrodriguez@users.noreply.github.com>
    Co-authored-by: Eric Rodriguez <feric.rodriguez@gmail.com>
    
    * fixed conflicts
    
    * validations
    
    Co-authored-by: alejandro <info@alezvi.com.ar>
    Co-authored-by: Eric Rodriguez <67911854+ericfrodriguez@users.noreply.github.com>
    Co-authored-by: Eric Rodriguez <feric.rodriguez@gmail.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,62 @@ const User = require('../models/User')
 const crypto = require('crypto') //recurso propio de nodeJS para generar códigos aleatorios y unicos
 const bcryptjs = require('bcryptjs') //recurso propio de nodeJS para hashear contraseñas
 const sendMail = require('./sendMail')
+const Joi = require('joi')
+
+const validator = Joi.object({
+    "name" : Joi.string()
+        .required()
+        .min(3)
+        .max(50)
+        .messages({
+            'any.required': 'NAME_REQUIRED',
+            'string.empty': 'NAME_REQUIRED',
+            'string.min': 'NAME_TOO_SHORT',
+            'string.max': 'NAME_TOO_LARGE',
+        }),
+    "photo" : Joi.string()
+        .required()
+        .uri()
+        .messages({
+            'any.required': 'PHOTO_REQUIRED',
+            'string.empty': 'PHOTO_REQUIRED',
+            'string.uri':'INVALID_URL'
+        }),
+    "email": Joi.string()
+        .required()
+        .email({minDomainSegments:2})    
+        .messages({
+            'any.required': 'EMAIL_REQUIRED',
+            'string.empty': 'EMAIL_REQUIRED',
+            'string.email': 'INVALID_EMAIL'
+        }),
+    "pass" : Joi.string()
+        .required()
+        .min(8)
+        .max(50)
+        .alphanum()
+        .messages({
+            'any.required': 'PASS_REQUIRED',
+            'string.empty': 'PASS_REQUIRED',
+            'string.min': 'PASS_TOO_SHORT',
+            'string.max': 'PASS_TOO_LARGE',
+            'string.alphanum': 'PASS_ALPHANUMERIC_REQUIRED',
+        }),
+    "role" : Joi.any()
+        .required()
+        .valid('user', 'admin')
+        .messages({
+            'any.required': 'ROLE_REQUIRED',
+            'string.empty': 'ROLE_REQUIRED',
+            'any.only': 'ROLE_NOT_ALLOWED'
+        }),
+    "from" : Joi.string()
+        .required()
+        .messages({
+            'any.required': 'FROM_REQUIRED',
+            'string.empty': 'FROM_REQUIRED'
+        }),
+})
 
 const userController = {
 
@@ -15,6 +71,7 @@ const userController = {
             from //el from tiene que venir desde el frontend para avisarle al método desde donde se crea el usuario
         } = req.body
         try {
+            await validator.validateAsync(req.body,{abortEarly:false})
             let user = await User.findOne({
                 email
             })
@@ -86,7 +143,7 @@ const userController = {
         } catch (error) {
             console.log(error)
             res.status(400).json({
-                message: "could't signed up",
+                message: error.message,
                 success: false
             })
         }
