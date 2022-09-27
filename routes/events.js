3bbb58016014e320c7af5d67d93809744f20e208
ignacioborraz <ignacioborraz@hotmail.com>
2022-09-27 16:41:14 -0300

    fixed passport

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var router = express.Router()
 let passport = require('../config/passport')
-let adminPassport = require('../config/adminPassport')
 
 const {
     all,
@@ -14,7 +13,7 @@ const {
 
 router.get('/', all)
 router.get('/:id', read)
-router.post('/', adminPassport.authenticate('jwt', {session:false}), create)
+router.post('/', passport.authenticate('jwt', {session:false}), create)
 router.patch('/:id', update)
 router.delete('/:id', destroy)
 router.patch('/like/:id', passport.authenticate('jwt', {session:false}), like)
