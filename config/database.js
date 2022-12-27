4ac0462724fe5abf7c27417a0dbae5105c63d889
Alejandro Villafañe <info@alezvi.com.ar>
2022-12-27 09:37:53 -0300

    improved comments and code style

--- config/database.js
+++ config/database.js
@@ -1,11 +1,29 @@
-require('dotenv').config()
-
 const {connect} = require('mongoose')
 
-connect(process.env.MONGO_URI, {
+/**
+ * Options for mongoose
+ * @type {{useUnifiedTopology: boolean, useNewUrlParser: boolean}}
+ */
+const options = {
+    /**
+     * @see {https://mongoosejs.com/docs/5.x/docs/deprecations.html#useunifiedtopology}
+     */
     useUnifiedTopology: true,
-    useNewUrlParser: true
-})
+
+    /**
+     * @see {https://mongoosejs.com/docs/5.x/docs/deprecations.html#the-usenewurlparser-option}
+     */
+    useNewUrlParser: true,
+}
+
+/**
+ * @see {https://mongoosejs.com/docs/connections.html}
+ *
+ * Connect to Mongo DB instance using URI provided in .env file
+ * This method returns  Promise but does nothing when connected
+ * Only when error occurs will call the catch callback function
+ */
+connect(process.env.MONGO_URI, options)
     .catch(error => {
         console.log('Error: ', error)
         process.exit(1)
